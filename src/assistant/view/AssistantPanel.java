3754bd3d9f9a5fe5023e84518fc2f64fca19bf96
smeth194243 <smor7432@740s-pl-114.local>
2017-01-10 13:50:49 -0700

    Started on adding panel and frame components

--- src/assistant/view/AssistantPanel.java
+++ src/assistant/view/AssistantPanel.java
@@ -8,7 +8,13 @@ import java.awt.Dimension;
 
 public class AssistantPanel extends JPanel  
 {
-	
+	//JTable- hold buttons
+	//when button clicked, edit button text
+	//make a "perform" button, that, when pressed, disables text editing and when the buttons are pressed, turns them green. when "perform" pressed again, it resets the color of all buttons back to gray and enables text editing again
+	private JButton button1;
+	private JButton button2;
+	private JButton button3;
+	private JButton button4;
 	
 	
 	public AssistantPanel(AssistantController baseController)
